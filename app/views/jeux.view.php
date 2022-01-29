8f9d2395535a3d3c070d26503ba7c37b127a2951
tintindrum <quentin-drumel@hotmail.com>
2022-01-29 21:25:10 +0100

    Maj_stylesetpage

--- app/views/jeux.view.php
+++ app/views/jeux.view.php
@@ -3,19 +3,48 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Forum</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="jeux.style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:ital@1&display=swap" rel="stylesheet">
 </head>
 <body>
     <header>
+        <div class="navbar">
+            <nav class="navigation hide" id="navigation">
+                <span class="close-icon" id="close-icon" onclick="showIconBar()"><i class="fa fa-close"></i></span>
+                <ul class="nav-list">
+                    <li class="nav-item"><a href="index.view.php">Accueil</a></li>
+                    <li class="nav-item"><a href="posts.html">Plateforme</a></li>
+                    <li class="nav-item"><a href="detail.html">Editeurs de Jeux</a></li>
+                </ul>
+            </nav>
+            <a class="bar-icon" id="iconBar" onclick="hideIconBar()"><i class="fa fa-bars"></i></a>
+            <div class="brand">Game Library</div>
+        </div>
+    
+    
         <div class="search-box">
             <div>
-                <select name="" id="">
-                    <option value="Everything">Plateforme</option>
-                    <option value="Titles">Jeux</option>
-                    <option value="Descriptions">Editeurs</option>
+                <select name="Plateforme" id="Search_plateforme">
+                    <option value="Plateforme1">Playstation</option>
+                    <option value="Plateforme2">Xbox</option>
+                    <option value="Plateforme3">Nintendo</option>
+                    <option value="Plateforme4">PC</option>
+                </select>
+                <select name="jouabilite" id="search_jouabilite">
+                    <option value="jouabilite1">Multijoueurs en ligne</option>
+                    <option value="jouabilite2">Coop en ligne</option>
+                    <option value="jouabilite3">Solo</option>
+                    <option value="jouabilite4">Multijoueurs local</option>
+                    <option value="jouabilite5">Coop local</option>
+                </select>
+                <select name="genre" id="">
+                    <option value="genre1">Action</option>
+                    <option value="genre2">Aventure</option>
+                    <option value="genre3">Stratégie</option>
+                    <option value="genre4">3D</option>
+                    <option value="genre5">Famille</option>
                 </select>
                 <input type="text" name="q" placeholder="search ...">
                 <button><i class="fa fa-search"></i></button>
@@ -23,5 +52,228 @@
         </div>
     </header>
 
+    <div class="container">
+        <div class="subforum">
+            <div class="subforum-title">
+                <h1>Jeux vidéo</h1>
+
+            </div>
+            <div class="subforum-row">
+                <div class="subforum-icon subforum-column center">
+                    <i class="fa fa-car center"></i>
+                </div>
+                <div class="subforum-description subforum-column">
+                    <h4><a href="#">Nom du jeux</a></h4>
+                    <p>Description Content: let's try to be cool, otherwise,w at 'sthe point in libing together with people youdont' live.</p>
+                </div>
+                
+                <div class="subforum-info subforum-column">
+                    <b><a href="">Sur Plateforme</a></b><br> par <a href="">Editeurs</a> 
+                    <br>on <small>12 Dec 2020</small>
+                </div>
+            </div>
+
+            <div class="subforum-row">
+                <div class="subforum-icon subforum-column center">
+                    <i class="fa fa-car center"></i>
+                </div>
+                <div class="subforum-description subforum-column">
+                    <h4><a href="#">Nom du jeux</a></h4>
+                    <p>Description Content: let's try to be cool, otherwise,w at 'sthe point in libing together with people youdont' live.</p>
+                </div>
+                
+                <div class="subforum-info subforum-column">
+                    <b><a href="">Sur Plateforme</a></b><br> par <a href="">Editeurs</a> 
+                    <br>on <small>12 Dec 2020</small>
+                </div>
+            </div>
+
+            <div class="subforum-row">
+                <div class="subforum-icon subforum-column center">
+                    <i class="fa fa-car center"></i>
+                </div>
+                <div class="subforum-description subforum-column">
+                    <h4><a href="#">Nom du jeux</a></h4>
+                    <p>Description Content: let's try to be cool, otherwise,w at 'sthe point in libing together with people youdont' live.</p>
+                </div>
+                
+                <div class="subforum-info subforum-column">
+                    <b><a href="">Sur Plateforme</a></b><br> par <a href="">Editeurs</a> 
+                    <br>on <small>12 Dec 2020</small>
+                </div>
+            </div>
+
+            <div class="subforum-row">
+                <div class="subforum-icon subforum-column center">
+                    <i class="fa fa-car center"></i>
+                </div>
+                <div class="subforum-description subforum-column">
+                    <h4><a href="#">Nom du jeux</a></h4>
+                    <p>Description Content: let's try to be cool, otherwise,w at 'sthe point in libing together with people youdont' live.</p>
+                </div>
+                
+                <div class="subforum-info subforum-column">
+                    <b><a href="">Sur Plateforme</a></b><br> par <a href="">Editeurs</a> 
+                    <br>on <small>12 Dec 2020</small>
+                </div>
+            </div>
+
+            <div class="subforum-row">
+                <div class="subforum-icon subforum-column center">
+                    <i class="fa fa-car center"></i>
+                </div>
+                <div class="subforum-description subforum-column">
+                    <h4><a href="#">Nom du jeux</a></h4>
+                    <p>Description Content: let's try to be cool, otherwise,w at 'sthe point in libing together with people youdont' live.</p>
+                </div>
+                
+                <div class="subforum-info subforum-column">
+                    <b><a href="">Sur Plateforme</a></b><br> par <a href="">Editeurs</a> 
+                    <br>on <small>12 Dec 2020</small>
+                </div>
+            </div>
+
+            <div class="subforum-row">
+                <div class="subforum-icon subforum-column center">
+                    <i class="fa fa-car center"></i>
+                </div>
+                <div class="subforum-description subforum-column">
+                    <h4><a href="#">Nom du jeux</a></h4>
+                    <p>Description Content: let's try to be cool, otherwise,w at 'sthe point in libing together with people youdont' live.</p>
+                </div>
+                
+                <div class="subforum-info subforum-column">
+                    <b><a href="">Sur Plateforme</a></b><br> par <a href="">Editeurs</a> 
+                    <br>on <small>12 Dec 2020</small>
+                </div>
+            </div>
+
+            <div class="subforum-row">
+                <div class="subforum-icon subforum-column center">
+                    <i class="fa fa-car center"></i>
+                </div>
+                <div class="subforum-description subforum-column">
+                    <h4><a href="#">Nom du jeux</a></h4>
+                    <p>Description Content: let's try to be cool, otherwise,w at 'sthe point in libing together with people youdont' live.</p>
+                </div>
+                
+                <div class="subforum-info subforum-column">
+                    <b><a href="">Sur Plateforme</a></b><br> par <a href="">Editeurs</a> 
+                    <br>on <small>12 Dec 2020</small>
+                </div>
+            </div>
+
+            <div class="subforum-row">
+                <div class="subforum-icon subforum-column center">
+                    <i class="fa fa-car center"></i>
+                </div>
+                <div class="subforum-description subforum-column">
+                    <h4><a href="#">Nom du jeux</a></h4>
+                    <p>Description Content: let's try to be cool, otherwise,w at 'sthe point in libing together with people youdont' live.</p>
+                </div>
+                
+                <div class="subforum-info subforum-column">
+                    <b><a href="">Sur Plateforme</a></b><br> par <a href="">Editeurs</a> 
+                    <br>on <small>12 Dec 2020</small>
+                </div>
+            </div>
+            
+            <div class="subforum-title">
+                <h1>Editeurs de jeux vidéo</h1>
+            </div>
+
+            <div class="subforum-row-edit">
+                <div class="subforum-icon subforum-column center">
+                    <h2>Nom de l'éditeur</h2>
+                </div>
+                <div class="subforum-description subforum-column">
+                    <h4><a href="#">Description </a></h4>
+                    <p>Description : Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia modi magni at eius officiis nihil laboriosam, fugit facilis fuga, voluptatibus sit rem mollitia saepe quisquam? Cumque natus suscipit quaerat quasi!</p><br>
+                    <h4>Jeux les plus vendu </h4>
+                    <p> Jeux les plus vendu : Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo possimus nesciunt eaque, voluptatum, maxime officia esse repellat sequi reiciendis obcaecati inventore. A vitae ut assumenda ipsa dolores cumque, reprehenderit consectetur.<p><br>
+                    <h3>Date de création<h3>
+                </div>
+             </div>
+
+             <div class="subforum-row-edit">
+                <div class="subforum-icon subforum-column center">
+                    <h2>Nom de l'éditeur</h2>
+                </div>
+                <div class="subforum-description subforum-column">
+                    <h4><a href="#">Description </a></h4>
+                    <p>Description : Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia modi magni at eius officiis nihil laboriosam, fugit facilis fuga, voluptatibus sit rem mollitia saepe quisquam? Cumque natus suscipit quaerat quasi!</p><br>
+                    <h4>Jeux les plus vendu </h4>
+                    <p> Jeux les plus vendu : Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo possimus nesciunt eaque, voluptatum, maxime officia esse repellat sequi reiciendis obcaecati inventore. A vitae ut assumenda ipsa dolores cumque, reprehenderit consectetur.<p><br>
+                    <h3>Date de création<h3>
+                </div>
+             </div>
+
+             <div class="subforum-row-edit">
+                <div class="subforum-icon subforum-column center">
+                    <h2>Nom de l'éditeur</h2>
+                </div>
+                <div class="subforum-description subforum-column">
+                    <h4><a href="#">Description </a></h4>
+                    <p>Description : Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia modi magni at eius officiis nihil laboriosam, fugit facilis fuga, voluptatibus sit rem mollitia saepe quisquam? Cumque natus suscipit quaerat quasi!</p><br>
+                    <h4>Jeux les plus vendu </h4>
+                    <p> Jeux les plus vendu : Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo possimus nesciunt eaque, voluptatum, maxime officia esse repellat sequi reiciendis obcaecati inventore. A vitae ut assumenda ipsa dolores cumque, reprehenderit consectetur.<p><br>
+                    <h3>Date de création<h3>
+                </div>
+             </div>
+
+             <div class="subforum-row-edit">
+                <div class="subforum-icon subforum-column center">
+                    <h2>Nom de l'éditeur</h2>
+                </div>
+                <div class="subforum-description subforum-column">
+                    <h4><a href="#">Description </a></h4>
+                    <p>Description : Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia modi magni at eius officiis nihil laboriosam, fugit facilis fuga, voluptatibus sit rem mollitia saepe quisquam? Cumque natus suscipit quaerat quasi!</p><br>
+                    <p> Jeux les plus vendu : Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo possimus nesciunt eaque, voluptatum, maxime officia esse repellat sequi reiciendis obcaecati inventore. A vitae ut assumenda ipsa dolores cumque, reprehenderit consectetur.<p><br>
+                    <h4>Jeux les plus vendu </h4>
+                    <h3>Date de création<h3>
+                </div>
+             </div>
+
+             <div class="subforum-row-edit">
+                <div class="subforum-icon subforum-column center">
+                    <h2>Nom de l'éditeur</h2>
+                </div>
+                <div class="subforum-description subforum-column">
+                    <h4><a href="#">Description </a></h4>
+                    <p>Description : Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia modi magni at eius officiis nihil laboriosam, fugit facilis fuga, voluptatibus sit rem mollitia saepe quisquam? Cumque natus suscipit quaerat quasi!</p><br>
+                    <h4>Jeux les plus vendu </h4>
+                    <p> Jeux les plus vendu : Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo possimus nesciunt eaque, voluptatum, maxime officia esse repellat sequi reiciendis obcaecati inventore. A vitae ut assumenda ipsa dolores cumque, reprehenderit consectetur.<p><br>
+                    <h3>Date de création<h3>
+                </div>
+             </div>
+
+
+            
+            
+        </div>
+
+        <!-- section jeux  -->
+
+
+
+
+
+
+        <script type="text/javascript">
+            
+            function hideIconBar(){
+        var iconBar = document.getElementById("iconBar");
+        var navigation = document.getElementById("navigation");
+        iconBar.setAttribute("style", "display:none;");
+        navigation.classList.remove("hide");
+        }
+
+        function showIconBar(){
+        var iconBar = document.getElementById("iconBar");
+        var navigation = document.getElementById("navigation");
+        iconBar.setAttribute("style", "display:block;");
+        navigation.classList.add("hide");
+        }
+        </script>
 </body>
 </html>
