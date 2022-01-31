626de3849ab1d13457e6e289491921763864a883
tintindrum <quentin-drumel@hotmail.com>
2022-01-31 10:55:56 +0100

    Maj_function

--- app/views/jeux.view.php
+++ app/views/jeux.view.php
@@ -1,3 +1,28 @@
+<?php 
+
+// on va chercher les jeux dans la base 
+
+require_once "../db/connDb.php";
+
+// requête 
+
+$sql = "SELECT * FROM Jeux ORDER BY 'id_jeux' DESC";
+
+//EXECUTE LA REQUETE 
+
+$requete = $pdo->query($sql);
+
+// on recupere les données 
+
+$Jeux = $requete->fetchAll();
+
+?>
+
+
+<?php foreach($Jeux as $Jeu): ?>
+
+<?php endforeach; ?>
+
 
 <html lang="fr">
 <head>
