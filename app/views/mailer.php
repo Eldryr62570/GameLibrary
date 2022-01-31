979ecf44163f318be13c974676a5ab73aa896d08
Eldryr62570 <jordan.moulin62570@gmail.com>
2022-01-31 10:55:58 +0100

    Commentaires

--- app/views/mailer.php
+++ app/views/mailer.php
@@ -1,7 +1,6 @@
 <?php
     session_start();
     require_once("../db/connDb.php");
-
     $_SESSION["verif"]= "";
     $to  = 'jordan.moulin62570@gmail.com'; 
     $subject = 'Ticket Joueur';
@@ -13,29 +12,23 @@
             $name = $_POST["name_mail"];
         }
     }
-    
-
     // Pour envoyer un mail HTML, l'en-tête Content-type doit être défini
     $headers[] = 'MIME-Version: 1.0';
     $headers[] = 'Content-type: text/html; charset=iso-8859-1';
     // En-têtes additionnels
     $headers[] = 'To: Jordan <jordan.moulin62570@gmail.com>';
     $headers[] = 'From: '.$name.' : <'.$email.'>';
-    
-
-    
     if(mail($to, $subject, $message, implode("\r\n", $headers))){
         $_SESSION["verif"] = true ;
     }
 
+
     // Envoi en bdd pour sauvegarder les logs des emails !
         // Ecriture de la requête
-        $sqlQuery = 'INSERT INTO mail(name_mail,email_mail,message_mail) VALUES (:name_mail,:email_mail,:message_mail)';
-    
+        $sqlQuery = 'INSERT INTO mail(name_mail,email_mail,message_mail) VALUES (:name_mail,:email_mail,:message_mail)';   
         // Préparation
-        $insertRecipe = $pdo->prepare($sqlQuery);
-    
-        // Exécution ! La recette est maintenant en base de données
+        $insertRecipe = $pdo->prepare($sqlQuery);   
+        // Exécution de la requête avec les variables provenant de notre formulaire
         $insertRecipe->execute([
             'name_mail' => $name,
             'email_mail' => $email,
