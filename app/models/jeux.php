431376519ef4b954c51339f30bfca8721f79f6ac
tintindrum <quentin-drumel@hotmail.com>
2022-01-31 10:40:50 +0100

    Maj

---
+++ app/models/jeux.php
@@ -0,0 +1,16 @@
+<?php 
+
+// on va chercher les jeux dans la base 
+
+require_once "../db/connDb.php";
+
+// requête 
+
+$sql = "SELECT * FROM Jeux ORDER BY 'id_jeux' DESC";
+
+//EXECUTE LA REQUETE 
+
+
+
+
+
